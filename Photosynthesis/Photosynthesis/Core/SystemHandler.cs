64eb5cc17bf34d1f0af363e1697a0655cc2803a0
DatYinYang <jaewonmichael@gmail.com>
2014-02-22 15:55:44 -0800

    Text Test
    Test of the text writer
    PreAlpha 0.1.0

--- Photosynthesis/Photosynthesis/Core/SystemHandler.cs
+++ Photosynthesis/Photosynthesis/Core/SystemHandler.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Photosynthesis.Core
 {
@@ -9,6 +13,12 @@ namespace Photosynthesis.Core
     {
         public List<String> CurrentText;
         public int TextIndex;
+        public SpriteFont TextFont;
+        public Vector2 TextLocation;
+        public int ButtonLag;
+
+        public SpriteBatch Batch;
+        public ContentManager Content;
 
         public enum Stage
         {
@@ -19,10 +29,71 @@ namespace Photosynthesis.Core
             PhotosystemOne
         };
 
+        public SystemHandler(SpriteBatch batch, ContentManager content)
+        {
+            this.Batch = batch;
+            this.Content = content;
+
+            this.TextFont = Content.Load<SpriteFont>("Font/TextFont");
+            this.CurrentText = null;
+            this.TextIndex = 0;
+            this.TextLocation = new Vector2(20, 550);
+            this.ButtonLag = 0;
+            
+            List<String> testText = new List<string>();
+            testText.Add("Hello");
+            testText.Add("Test");
+            testText.Add("Bye");
+            this.DrawText(testText);
+        }
+
+        public void Draw()
+        {
+            this.Batch.Begin();
+
+            this.DrawStringOnScreen(this.TextLocation);
+
+            this.Batch.End();
+        }
+
+        public void Update(MouseState mState, KeyboardState kState)
+        {
+            this.UpdateText(mState, kState);
+        }
+
         public void DrawText(List<String> text)
         {
             this.CurrentText = text;
             this.TextIndex = 0;
+        }
+
+        public void UpdateText(MouseState mState, KeyboardState kState)
+        {
+            if (this.ButtonLag > 0)
+            {
+                this.ButtonLag--;
+            }
+            else 
+            {
+                if (mState.LeftButton == ButtonState.Pressed || kState.IsKeyDown(Keys.Enter))
+                {
+                    this.TextIndex++;
+                    this.ButtonLag = 20;
+                    if (this.TextIndex >= this.CurrentText.Count)
+                    {
+                        this.CurrentText = null;
+                        this.TextIndex = 0;
+                    }
+                }
+            }
+        }
+
+        public void DrawStringOnScreen(Vector2 location)
+        {
+            if (this.CurrentText != null)
+            {
+                this.Batch.DrawString(TextFont, this.CurrentText[this.TextIndex], location, Color.Black);
+            }
         }
     }
 }
